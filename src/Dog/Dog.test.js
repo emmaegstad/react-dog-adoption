a54bf6697f85521ec4994441222f84c325b82a67
Emma Egstad <emma@egstad.com>
2022-01-04 14:12:07 -0800

    passing tests

---
+++ src/Dog/Dog.test.js
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import DogDetail from '../DogDetail/DogDetail';
+import { MemoryRouter } from 'react-router-dom';
+
+test('renders the DogDetail component', () => {
+  const container = render(
+    <MemoryRouter>
+      <DogDetail
+        dog={[
+          {
+            name: 'Althea',
+            image: 'https://placedog.net/500?id=10',
+            bio: 'High energy pooch! Would do well in an active family with another dog to keep her company.',
+          },
+        ]}
+      />
+    </MemoryRouter>
+  );
+  expect(container).toMatchSnapshot();
+});
